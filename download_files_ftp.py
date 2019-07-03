861789d9ef84b686331c278517cd3f669d8e4fcb
Simon Jackson <simon.robin.jackson@gmail.com>
2019-07-03 12:28:51 +1000

    Start Stop services
    Updated to stop/start image service as part of update/sync

--- download_files_ftp.py
+++ download_files_ftp.py
@@ -3,6 +3,18 @@ import os, sys
 import gzip
 import shutil
 import arcpy
+from arcgis.gis import GIS
+import arcgis.gis.admin
+
+# =============================================================================
+# Outstanding tasks:
+#     Publish an Image Web Layer that references the Mosaic Dataset & Processing Templates
+#     Setup pretty processing templates
+#     Incorporate downloading IDZ71071_AUS_WindMagKmh_SFC.nc + IDZ71089_AUS_Wind_Dir_SFC.nc
+#     Add/remove/sync above two similar to how T_SFC is done below
+#     Ensure all files are being deleted + look into clearing MD Log file Guenter mentioned
+#     Deploy & test on Esri Australia Demo site
+# =============================================================================
 
 # Globals
 host = 'ftp.bom.gov.au'
@@ -11,18 +23,21 @@ source_dir = '/register/bom630/adfd'
 filename = 'IDZ71000_AUS_T_SFC.nc.gz'  
 username = ''
 password = ''
-dest_dir = r"D:\Work\BOM\download_from_ftp"
+dest_dir = r"E:\ArcGIS_Projects\BOM\download_from_ftp"  #ToDo: Make relative
 arcpy.env.workspace = os.path.join(os.path.dirname(sys.argv[0]), "BOM_FTP.gdb")
-
+gis = GIS("https://simongis.yourdomain.com.au/portal", "", "")
 
 def main():
+    # Stop service
+    stop()
     # download file from FTP
     downloadFile()
     # unzip file and delete zip
     unZip()
     # sync mosaic dataset
     syncMD()
-
+    #start service
+    start()
     print('Finished')
 
 def downloadFile():
@@ -50,7 +65,6 @@ def downloadFile():
 
 def unZip():
     print("unzipping: " + filename)
-    # Should we not delete the existing NetCDF with same filename before unzipping?
     # Extract zip
     with gzip.open(os.path.join(dest_dir, filename), 'rb') as infile, open(os.path.join(dest_dir, filename[:-3]), 'wb') as outfile:
         shutil.copyfileobj(infile, outfile)
@@ -60,8 +74,10 @@ def unZip():
     os.remove(os.path.join(dest_dir, filename))
     print(filename, ' deleted')
 
-def syncMD():    
-    print("Removing Rasters & Footprints")
+def syncMD():
+    # ToDo: STOP image service that references this MD
+    #stop()    
+    print("Removing Rasters & Footprints")  # Was hoping to avoid removing/adding rasters, but Hong thinks there is a bug with SyncMD in Pro
     arcpy.management.RemoveRastersFromMosaicDataset("T_SFC", "OBJECTID >= 0", "UPDATE_BOUNDARY", "MARK_OVERVIEW_ITEMS", "DELETE_OVERVIEW_IMAGES", "DELETE_ITEM_CACHE", "REMOVE_MOSAICDATASET_ITEMS", "UPDATE_CELL_SIZES")
     print("Removed Rasters & Footprints")
     print("Adding Rasters: " + filename[:-3])
@@ -69,7 +85,25 @@ def syncMD():
     print("Finished adding " + filename[:-3])
     print("syncing mosaic dataset")
     arcpy.management.SynchronizeMosaicDataset("T_SFC", None, "UPDATE_WITH_NEW_ITEMS", "SYNC_STALE", "UPDATE_CELL_SIZES", "UPDATE_BOUNDARY", "NO_OVERVIEWS", "NO_PYRAMIDS", "NO_STATISTICS", "NO_THUMBNAILS", "NO_ITEM_CACHE", "REBUILD_RASTER", "UPDATE_FIELDS", "CenterX;CenterY;Dimensions;GroupName;ProductName;Raster;Shape;StdTime;Tag;Variable;ZOrder", "IGNORE_EXISTING_ITEMS", "IGNORE_BROKEN_ITEMS", "SKIP_EXISTING_ITEMS", "REFRESH_INFO", "NO_STATISTICS")
-    print('Finished syncing Mosaic Dataset')
+    print('Finished syncing Mosaic Dataset') 
+    # ToDo: START image service that references this MD
+    #start()
+
+def stop():
+    # Get the ArcGIS Server federated to the Portal (might need to check this when other sites are federated)
+    gis_server = gis.admin.servers.list()[0]
+    for service in gis_server.services.list(folder='BOM'):
+        print("Stopping ", service.properties.serviceName)
+        service.stop() 
+        print("Stopped ", service.properties.serviceName)
+        
+def start():
+    # Get the ArcGIS Server federated to the Portal (might need to check this when other sites are federated)
+    gis_server = gis.admin.servers.list()[0]
+    for service in gis_server.services.list(folder='BOM'):
+        print("Starting ", service.properties.serviceName)
+        service.start()
+        print("Started ", service.properties.serviceName)
 
 if __name__ == '__main__':
     main()
